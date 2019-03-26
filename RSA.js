f9c85678642dffe9bc142ef89cde59397474cb84
Olaf de Rohan Willner <olafdrw@gmail.com>
2019-03-26 21:41:34 +0000

    Update RSA.js

--- RSA.js
+++ RSA.js
@@ -1,5 +1,24 @@
 // Olaf Willner, 2019
 
-function generate_keypair(p, q) {
-  var n = document.getElementByName("")
+function isPrime(num) {
+  for(var i = 2; i < num; i++)
+    if(num % i === 0) return false;
+  return num > 1;
+}
+
+var p = document.getElementByName("num1")
+var q = document.getElementByName("num2")
+
+function phi(p,q) {
+  phi = (p-1)(q-1);
+  return phi;
+}
+
+function generate_keypair(p,q) {
+  var n = p * q;
+  var e = Math.floor((Math.random() * phi(p,q)) + 1);
+  var k = 2
+  var d = (k*phi(p,q) + 1) / e
+  
+  return
 }
