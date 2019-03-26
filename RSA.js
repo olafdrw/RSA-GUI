facdf221988f770269de63e2da85162adc5b6d3d
Olaf de Rohan Willner <olafdrw@gmail.com>
2019-03-26 13:33:37 +0000

    Update RSA.js

--- RSA.js
+++ RSA.js
@@ -2,31 +2,31 @@
 
 function gcd(a, b) {
   while (b != 0) {
-    a, b = b, a % b;
+    var a, b = b, a % b;
   }
   return a;
 }
 
 function multiplicative_inverse(e, phi){
-    d = 0;
-    x1 = 0;
-    x2 = 1;
-    y1 = 1;
-    temp_phi = phi;
+    var d = 0;
+    var x1 = 0;
+    var x2 = 1;
+    var y1 = 1;
+    var temp_phi = phi;
 
     while (e > 0:) {
-        temp1 = temp_phi/e;
-        temp2 = temp_phi - temp1 * e;
-        temp_phi = e;
-        e = temp2;
+        var temp1 = temp_phi/e;
+        var temp2 = temp_phi - temp1 * e;
+        var temp_phi = e;
+        var e = temp2;
 
-        x = x2- temp1* x1;
-        y = d - temp1 * y1;
+        var x = x2- temp1* x1;
+        var y = d - temp1 * y1;
 
-        x2 = x1;
-        x1 = x;
-        d = y1;
-        y1 = y;
+        var x2 = x1;
+        var x1 = x;
+        var d = y1;
+        var y1 = y;
     }
 
     if (temp_phi == 1) {
@@ -47,4 +47,33 @@ function is_prime(num){
         }
     return True;
     }
+}
+
+function generate_keypair(p, q) {
+    if (not (is_prime(p) and is_prime(q))) {
+        raise ValueError('Both numbers must be prime.');
+    }
+    elif (p == q) {
+        raise ValueError('p and q cannot be equal');
+    }
+    var n = p * q;
+
+    var phi = (p-1) * (q-1);
+
+    // Choose an integer e such that e and phi(n) are coprime
+    var e = random.randrange(1, phi);
+
+    // Use Euclid's Algorithm to verify that e and phi(n) are comprime
+    var g = gcd(e, phi);
+    while (g != 1) {
+        var e = random.randrange(1, phi);
+        var g = gcd(e, phi);
+    }
+
+    // Use Extended Euclid's Algorithm to generate the private key
+    var d = multiplicative_inverse(e, phi);
+
+    // Return public and private keypair
+    // Public key is (e, n) and private key is (d, n)
+    return ((e, n), (d, n));
 }
