b082051f60d0c5a47704b4e8980e405c2b791fe2
Olaf de Rohan Willner <olafdrw@gmail.com>
2019-03-30 20:36:45 +0000

    update

--- RSA.js
+++ RSA.js
@@ -1,26 +1,44 @@
 // Olaf Willner, 2019
 
-function isPrime(num) {
-  for(var i = 2; i < num; i++)
-    if(num % i === 0) return false;
-  return num > 1;
+function get_value(id) {
+    return parseFloat(document.getElementById(id).value);
 }
 
-var p = document.getElementByName("num1");
-var q = document.getElementByName("num2");
-
-function phi(p,q) {
-  phi = (p-1)(q-1);
-  return phi;
+function multiply() {
+  var p = document.getElementsById("num1").value;
+  var q = document.getElementsById("num2").value;
+  var calc = p*q;
+  return calc;
 }
 
-function generate_keypair(p,q) {
-  var n = p * q;
-  var text = "";
-  var e = Math.floor((Math.random() * phi(p,q)) + 1);
-  var k = 2;
-  var d = (k*phi(p,q) + 1) / e;
-  return n
-  }
-  document.write(generate_keypair(p,q));
-}
+
+
+
+
+
+
+
+
+
+
+//function isPrime(num) {
+  //for(var i = 2; i < num; i++) {
+    //if(num % i === 0) return false;
+  //}
+  //return num > 1;
+//}
+
+//function phi(p,q) {
+  //phi = (p-1)(q-1);
+  //return phi;
+//}
+
+//function generate_keypair(p,q) {
+  //var n = p * q;
+  //var text = "";
+  //var e = Math.floor((Math.random() * phi(p,q)) + 1);
+  //var k = 2;
+  //var d = (k*phi(p,q) + 1) / e;
+  //return n;
+  //document.write(generate_keypair(p,q)).innerText;
+//}
