8000a289c78ed3c1f3d05faa34026daddfe50810
Olaf de Rohan Willner <olafdrw@gmail.com>
2019-03-31 14:47:44 +0100

    Update RSA.js

--- RSA.js
+++ RSA.js
@@ -4,42 +4,9 @@ function generate_publicKey() {
   var p = document.getElementById("num1").value;
   var q = document.getElementById("num2").value;
   var n = p*q;
-  var e = 3;
+  var e = math.pow(2,16) + 1;
   var phi = (p-1)*(q-1);
   var d = (math.pow(e,-1)) % phi
+  var
   document.getElementById("output").textContent = "Public Key = (" + n + "," + e + ")";
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-//function isPrime(num) {
-  //for(var i = 2; i < num; i++) {
-    //if(num % i === 0) return false;
-  //}
-  //return num > 1;
-//}
-
-//function phi(p,q) {
-  //phi = (p-1)(q-1);
-  //return phi;
-//}
-
-//function generate_keypair(p,q) {
-  //var n = p * q;
-  //var text = "";
-  //var e = Math.floor((Math.random() * phi(p,q)) + 1);
-  //var k = 2;
-  //var d = (k*phi(p,q) + 1) / e;
-  //return n;
-  //document.write(generate_keypair(p,q)).innerText;
-//}
