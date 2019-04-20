97ea170731701042ebbcc921c41733cca92e0ad2
Olaf de Rohan Willner <olafdrw@gmail.com>
2019-04-20 19:47:57 +0100

    update

--- RSA.js
+++ RSA.js
@@ -1,11 +1,12 @@
 // Olaf Willner, 2019
 
-function generate_publicKey() {
+function generate_keyPair() {
   var p = document.getElementById("num1").value;
   var q = document.getElementById("num2").value;
   var n = p*q;
   const e = Math.pow(2,16) + 1;
   var phi = (p-1)*(q-1);
   var d = (Math.pow(e,-1)) % phi;
-  document.getElementById("output").innerHTML = "Public Key = (<strong>" + n + "</strong>,<strong>" + e + "</strong>)";
+  document.getElementById("output1").innerHTML = "Public Key = (<strong>" + n + "</strong>,<strong>" + e + "</strong>)";
+  document.getElementById("output2").innerHTML = "Private Key = (<strong>" + d + "</strong>)";
 }
