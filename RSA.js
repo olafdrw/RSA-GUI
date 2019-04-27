61075b78281fd989d302c8b41e72fb06c07211ec
Olaf de Rohan Willner <olafdrw@gmail.com>
2019-04-27 13:01:52 +0100

    update ALL WORKS!!

--- RSA.js
+++ RSA.js
@@ -1,12 +1,21 @@
 // Olaf Willner, 2019
 
+var modInverse = function(a, b) {
+    a %= b;
+    for (var x = 1; x < b; x++) {
+        if ((a*x)%b == 1) {
+            return x;
+        }
+    }
+}
+
 function generate_keyPair() {
   var p = document.getElementById("num1").value;
   var q = document.getElementById("num2").value;
   var n = p*q;
   const e = Math.pow(2,16) + 1;
   var phi = (p-1)*(q-1);
-  var d = (Math.pow(e,-1)) % phi;
+  var d = modInverse(e, phi);
   document.getElementById("output1").innerHTML = "Public Key = (<strong>" + n + "</strong>,<strong>" + e + "</strong>)";
   document.getElementById("output2").innerHTML = "Private Key = (<strong>" + d + "</strong>)";
 }
