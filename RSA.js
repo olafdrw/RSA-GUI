bc858ff89eba14ac629cdd6cc1b66e21082987b4
Olaf Willner <olafdrw@gmail.com>
2019-07-29 16:24:58 +0100

    Update RSA.js

--- RSA.js
+++ RSA.js
@@ -23,22 +23,23 @@ function generate_keyPair() {
 }
 
 function letterValue(str){
-    var anum={
-        a: 1, b: 2, c: 3, d: 4, e: 5, f: 6, g: 7, h: 8, i: 9, j: 10, k: 11,
-        l: 12, m: 13, n: 14,o: 15, p: 16, q: 17, r: 18, s: 19, t: 20,
-        u: 21, v: 22, w: 23, x: 24, y: 25, z: 26
+    if(str.length== 1) {
+      if(str >= "a" && str <= "z")
+        return str.charCodeAt(0) - 'a'.charCodeAt(0) + 1;
+      else
+        return '';
     }
-    if(str.length== 1) return anum[str] || ' ';
     return str.split('').map(letterValue);
 }
 
-function encryptText() {
-  var p = document.getElementById("num1").value;
-  var q = document.getElementById("num2").value;
+function encryptText(text) {
+  var p = BigInt(document.getElementById("num1").value);
+  var q = BigInt(document.getElementById("num2").value);
   var n = p*q;
-  const e = Math.pow(2, 16) + 1;
-  var plaintext = document.getElementById('plaintext');
-  var c = letterValue(String(plaintext));
-  var ciphertext = c.map((el) => Math.pow(el, e) % n);
-  document.getElementById("output3").innerHTML = "Encrypted text = " + ciphertext;
+  const e = 2n ** 16n + 1n;
+  var c = letterValue(String(text));
+  var ciphertext = c.map(el => (BigInt(el) ** e) % n);
+  return ciphertext;
 }
+
+console.log(encryptText("abc").map(e => e.toString()));
